91737f1abebe42cfcad11a5585459945b5341c9e
unknown <kaa9768@nyu.edu>
2025-09-22 13:29:17 +0400

    Added extra tests to complete coverage

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -5,8 +5,27 @@ from bank_account.bank_account import BankAccount
 @pytest.fixture
 def start_account():
     return BankAccount(100)
-
+def test_account():
+    return BankAccount(100)
+x=2
 
 def test_deposit(start_account):
     start_account.deposit(50)
     assert start_account.balance == 150
+def test_failed_deposit(start_account):
+    start_account.deposit(-35)
+
+def test_withdraw(start_account):
+    start_account.withdraw(50)
+def test_failed_withdraw(start_account):
+    start_account.withdraw(1000000)
+def test_failed_withdraw_neg(start_account):
+    start_account.withdraw(-50)
+def test_transfer(start_account):
+    start_account.transfer_to(test_account,50)
+def test_transfer(start_account):
+    start_account.transfer_to(x,50)
+
+
+
+
