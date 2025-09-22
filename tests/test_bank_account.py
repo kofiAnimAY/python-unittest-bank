30e717e3824fcfab585b582ea43d3cf1ddc93c48
unknown <kaa9768@nyu.edu>
2025-09-22 13:42:06 +0400

    Fixed tests

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -13,7 +13,7 @@ def test_deposit(start_account):
     start_account.deposit(50)
     assert start_account.balance == 150
 def test_failed_deposit(start_account):
-    with pytest.raises(ValueError("Deposit amount must be positive")):
+    with pytest.raises(ValueError):
         start_account.deposit(-35)
     
 def test_withdraw(start_account):
@@ -21,16 +21,16 @@ def test_withdraw(start_account):
     start_account.withdraw(50)
     assert start_account.balance==50
 def test_failed_withdraw(start_account):
-    with pytest.raises(ValueError("Insufficient funds")):
+    with pytest.raises(ValueError):
         start_account.withdraw(1000000)
 def test_failed_withdraw_neg(start_account):
-    with pytest.raises(ValueError("Withdraw amount must be positive")):
+    with pytest.raises(ValueError):
         start_account.withdraw(-50)
 def test_transfer(start_account,test_account):
     start_account.transfer_to(test_account,50)
     assert test_account.balance==150 and start_account.balance==50
 def test_failed_transfer(start_account,x):
-    with pytest.raises(ValueError("Target must be a BankAccount")):
+    with pytest.raises(ValueError):
         start_account.transfer_to(x,50)
 
 
