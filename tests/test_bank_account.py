4c3f538ef54cda140914f11ea47a44ab0dc6875d
unknown <kaa9768@nyu.edu>
2025-09-24 13:38:49 +0400

    Testing CI

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -11,7 +11,7 @@ def test_account():
 x=2
 
 def test_deposit(start_account):
-    start_account.deposit(50)
+    start_account.deposit(60)
     assert start_account.balance == 150
 def test_failed_deposit(start_account):
     with pytest.raises(ValueError):
